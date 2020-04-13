cdc3bd0befa9af13cd12e275ee44caebbc72c737
HUSSAMFAEZALTARAJI <60089237+HUSSAMFAEZALTARAJI@users.noreply.github.com>
2020-04-13 16:42:39 +0300

    Update conditional .py

--- conditional .py
+++ conditional .py
@@ -1,12 +1,19 @@
-Age = input("Enter your age :")
+# Age = input("Enter your age :")
 
-if int(Age)>=5 and int(Age)<=10 :
-    print("kids ")
-elif int(Age)>=11 and int(Age)<=15 :
-    print("Teenager ")
-elif int(Age)>=16 and int(Age)<=18 : 
-    print("youth")
-else :
-    print("Adult")    
+# if int(Age)>18 :
+#     print("wlecom ")
+
+# else :
+#     print("No Enter")    
 
 
+# Age = input("Enter your age :")
+
+# if int(Age)>=5 and int(Age)<=10 :
+#     print("kids ")
+# elif int(Age)>=11 and int(Age)<=15 :
+#     print("Teenager ")
+# elif int(Age)>=16 and int(Age)<=18 : 
+#     print("youth")
+# else :
+#     print("Adult")    
