a69912df40e8e0d8b5ebba009f263d498262fdcc
HUSSAMFAEZALTARAJI <60089237+HUSSAMFAEZALTARAJI@users.noreply.github.com>
2020-04-17 13:37:28 +0300

    Add files via upload
    print("Wlecom")
    I = 0
    while (I<10) :
       if (I==5):
        I+=1
        break  
       
       print("How are you {}".format(I))
       I+=1
    print("End program")    
    
    print("===============================================================")
    print("Wlecom")
    I = 0
    while (I<10) :
       if (I==5):
        I+=1
        continue  
       
       print("How are you {}".format(I))
       I+=1
    print("End program")

---
+++ While loop break&continue .py
@@ -0,0 +1,26 @@
+print("Wlecom")
+I = 0
+while (I<10) :
+   if (I==5):
+    I+=1
+    break  
+   
+   print("How are you {}".format(I))
+   I+=1
+print("End program")    
+
+print("===============================================================")
+print("Wlecom")
+I = 0
+while (I<10) :
+   if (I==5):
+    I+=1
+    continue  
+   
+   print("How are you {}".format(I))
+   I+=1
+print("End program")    
+
+
+
+
