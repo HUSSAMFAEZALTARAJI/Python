d26929e3bdf614b6ac7f0cc40cfe73c23eef91ed
HUSSAMFAEZALTARAJI <60089237+HUSSAMFAEZALTARAJI@users.noreply.github.com>
2020-04-20 15:06:58 +0300

    Add files via upload
    # for x in range(0,10,2):
    #     print("wlecom",x,(x+1)*".")
    
    # for p in "python" :
    #     print(p)
    
    
    # for x in range(10) :
    #     for y in range(10) :
    #         print(f"({x},{y})")

---
+++ for loopping .py
@@ -0,0 +1,12 @@
+# for x in range(0,10,2):
+#     print("wlecom",x,(x+1)*".")
+
+# for p in "python" :
+#     print(p)
+
+
+# for x in range(10) :
+#     for y in range(10) :
+#         print(f"({x},{y})")
+
+
