69be8d8322e34adcb4394d409526a60abc7e6b30
HUSSAMFAEZALTARAJI <60089237+HUSSAMFAEZALTARAJI@users.noreply.github.com>
2020-04-21 17:17:40 +0300

    Add files via upload
    # print("start programe ")
    # count = 0
    # for number in range(int(input("please , enter your range :"))) :
    #     if number %2 == 0 :
    #         count +=1
    #         print(number)
    # print(f"We have {count} even number")        
    
    
    
    # Hussam = False
    # count = 0
    
    # for number in range(int(input("please , enter your range :"))) :
    #     # print("Wlecom ")
    #     if Hussam :
    #         print("Hello, in our website")
    #         break
    #     else:
    #         count += 1
    #         print(f"Wlecom, you register more than {count} time fail")

---
+++ app of for .py
@@ -0,0 +1,21 @@
+# print("start programe ")
+# count = 0
+# for number in range(int(input("please , enter your range :"))) :
+#     if number %2 == 0 :
+#         count +=1
+#         print(number)
+# print(f"We have {count} even number")        
+
+
+
+Hussam = False
+count = 0
+
+for number in range(int(input("please , enter your range :"))) :
+    # print("Wlecom ")
+    if Hussam :
+        print("Hello, in our website")
+        break
+    else:
+        count += 1
+        print(f"Wlecom, you register more than {count} time fail")
