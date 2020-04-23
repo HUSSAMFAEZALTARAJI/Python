257f01c5ae3ba1e3653cd9008631b6092f488e7c
HUSSAMFAEZALTARAJI <60089237+HUSSAMFAEZALTARAJI@users.noreply.github.com>
2020-04-23 15:08:10 +0300

    Add files via upload
    # def greet() :
    #     print("Hi guys")
    #     print("welcom with us in our lesson")
    # greet()    
    
    
    
    # def name(first_name, Last_name) :
    #     print(f"Hi {first_name} {Last_name}")
    #     print("welcom with us in our lesson")
    # name(str(input("Enter your first name :")),str(input("Enter your Last name :")))

---
+++ DEF part1.py
@@ -0,0 +1,12 @@
+# def greet() :
+#     print("Hi guys")
+#     print("welcom with us in our lesson")
+# greet()    
+
+
+
+# def name(first_name, Last_name) :
+#     print(f"Hi {first_name} {Last_name}")
+#     print("welcom with us in our lesson")
+# name(str(input("Enter your first name :")),str(input("Enter your Last name :")))    
+
