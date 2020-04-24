d549ef38808bae7fce2b33be6fef50f2e01993e6
HUSSAMFAEZALTARAJI <60089237+HUSSAMFAEZALTARAJI@users.noreply.github.com>
2020-04-24 14:18:57 +0300

    Add files via upload
    # def name(first_name, Last_name):
    #     return f"Hi {first_name} {Last_name} welcom with us in our lesson"
    # print(name(str(input("Enter your first name :")),str(input("Enter your Last name :")))) 
    # message=name(str(input("Enter your first name :")),str(input("Enter your Last name :")))    
    # file = open('C:/Users/hussa/OneDrive/Desktop/New Text Document (2).txt', 'w')
    # file.write(message)
    
    
    
    # def plus (x, y) :
    #     return x+y
    # print(plus(2,1))    
    
    
    # def muliplay (*numbers) :
    #     totall =1
    #     for number in numbers :
    #         totall*= number
    #     return totall
    # print(muliplay(5,4,2,6,8,7))   
    
    
    
    
    # def add_number(x,y,z=5) :
    #     if z==5:
    #         return x*y
    #     else :
    #         return z+x*y    
    # print(add_number(5,4,2))

---
+++ DEF part 2.py
@@ -0,0 +1,30 @@
+# def name(first_name, Last_name):
+#     return f"Hi {first_name} {Last_name} welcom with us in our lesson"
+# print(name(str(input("Enter your first name :")),str(input("Enter your Last name :")))) 
+# message=name(str(input("Enter your first name :")),str(input("Enter your Last name :")))    
+# file = open('C:/Users/hussa/OneDrive/Desktop/New Text Document (2).txt', 'w')
+# file.write(message)
+
+
+
+# def plus (x, y) :
+#     return x+y
+# print(plus(2,1))    
+
+
+# def muliplay (*numbers) :
+#     totall =1
+#     for number in numbers :
+#         totall*= number
+#     return totall
+# print(muliplay(5,4,2,6,8,7))   
+
+
+
+
+# def add_number(x,y,z=5) :
+#     if z==5:
+#         return x*y
+#     else :
+#         return z+x*y    
+# print(add_number(5,4,2))
