273f9d6646119042fb23b6ceed429a5168b1e5d5
HUSSAMFAEZALTARAJI <60089237+HUSSAMFAEZALTARAJI@users.noreply.github.com>
2020-04-25 12:39:06 +0300

    Add files via upload
    # def name(first_name, Last_name):
    #     return f"Hi {first_name} {Last_name} welcom with us in our lesson"
    # print(name(str(input("Enter your first name :")),str(input("Enter your Last name :")))) 
    # message=name(str(input("Enter your first name :")),str(input("Enter your Last name :")))    
    # file = open('C:/Users/hussa/OneDrive/Desktop/New Text Document (2).txt', 'w')
    # file.write(message)
    
    
    # f = open(r'C:/Users/hussa/OneDrive/Desktop/youtube chanel/New Text Document (4).txt','r')
    # if f.mode == 'r':
    #     hh = f.read()
    #     print(hh)

---
+++ open function.py
@@ -0,0 +1,12 @@
+# def name(first_name, Last_name):
+#     return f"Hi {first_name} {Last_name} welcom with us in our lesson"
+# print(name(str(input("Enter your first name :")),str(input("Enter your Last name :")))) 
+# message=name(str(input("Enter your first name :")),str(input("Enter your Last name :")))    
+# file = open('C:/Users/hussa/OneDrive/Desktop/New Text Document (2).txt', 'w')
+# file.write(message)
+
+
+# f = open(r'C:/Users/hussa/OneDrive/Desktop/youtube chanel/New Text Document (4).txt','r')
+# if f.mode == 'r':
+#     hh = f.read()
+#     print(hh)
